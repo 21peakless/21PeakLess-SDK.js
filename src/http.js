287b0eec8c05671cdc1afaa7fad75ec73c8c5f1c
Jan Biasi <jan.biasi@namics.com>
2017-09-29 14:43:10 +0200

    Update build with fixes

--- src/http.js
+++ src/http.js
@@ -5,17 +5,19 @@ const createXHRClient = (config = {}) => {
     const { insecure, host, token, version, workspace } = config;
     const protocol = !!insecure ? 'http' : 'https';
 
-    axios.create({
+    const client = axios.create({
         baseURL: `${protocol}://${host}/api/v${version}/workspace/${workspace}`,
     });
 
-    defaults.headers.common = {
+    client.defaults.headers.common = {
         Accept: 'application/json, text/plain, */*',
         'Accept-Encoding': 'gzip, deflate, br',
         Connection: 'keep-alive',
         Authorization: token,
         'X-21PeakLess-Agent': `@21peakless/sdk#${pkg.version}`,
     };
+
+    return client;
 };
 
 export default createXHRClient;
--- webpack.config.js
+++ webpack.config.js
@@ -35,9 +35,6 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         }),
-        new webpack.SourceMapDevToolPlugin({
-            filename: '[name].sdk.js.map',
-        }),
         new webpack.optimize.ModuleConcatenationPlugin(),
     ],
     devServer: {
